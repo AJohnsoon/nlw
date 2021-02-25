82dce23b81a3efad18525db6db0dc101d065461c
Allan Leal <allanjohnson.leal@gmail.com>
2021-02-24 22:10:49 -0300

    create a Unity Test

---
+++ src/__tests__/User.test.ts
@@ -0,0 +1,31 @@
+import request from 'supertest';
+import { app } from '../app';
+import createConnection from '../database';
+
+
+describe("Users", () => {
+    beforeAll(async () => {
+        const connection = await createConnection()
+        await connection.runMigrations();
+    });
+
+    it("should be able to create a ew user", async () => {
+        const response = await request(app).post("/users").send({
+            email: "userExample@exemple.com",
+            name: "My Example",
+        });
+
+        expect(response.status).toBe(201);
+    });
+
+
+    it("should not be able to create a user with exists email", async () => {
+        const response = await request(app).post("/users").send({
+            email: "userExample@exemple.com",
+            name: "My Example",
+        });
+
+        expect(response.status).toBe(400);
+    });
+
+});
